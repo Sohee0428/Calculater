128ff349b4ea2d166d22407137dc6aeaf028a0aa
Sohee <sohee3480@gmail.com>
2021-08-17 22:08:54 +0900

    각 버튼에 숫자 부여하기

--- app/src/main/java/com/example/aop_part2_chapter04/MainActivity.kt
+++ app/src/main/java/com/example/aop_part2_chapter04/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.aop_part2_chapter04
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,19 +12,48 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun buttonClicked(v: View){
+    fun buttonClicked(v: View) {
+        when (v.id) {
+
+            R.id.Btn0 -> numberButtonClicked("0")
+            R.id.Btn1 -> numberButtonClicked("1")
+            R.id.Btn2 -> numberButtonClicked("2")
+            R.id.Btn3 -> numberButtonClicked("3")
+            R.id.Btn4 -> numberButtonClicked("4")
+            R.id.Btn5 -> numberButtonClicked("5")
+            R.id.Btn6 -> numberButtonClicked("6")
+            R.id.Btn7 -> numberButtonClicked("7")
+            R.id.Btn8 -> numberButtonClicked("8")
+            R.id.Btn9 -> numberButtonClicked("9")
+            R.id.BtnPlus -> operatorButtonClicked("+")
+            R.id.BtnMinus -> operatorButtonClicked("-")
+            R.id.BtnMulti -> operatorButtonClicked("*")
+            R.id.BtnDivider -> operatorButtonClicked("/")
+            R.id.BtnModulo -> operatorButtonClicked("%")
+
+        }
 
     }
 
-    fun resultButtonClicked(v: View){
+    private fun numberButtonClicked(number: String) {
 
     }
 
-    fun historyButtonClicked(v: View){
+    private fun operatorButtonClicked(operator: String) {
 
     }
 
-    fun clearButtonClicked(v: View){
+    fun resultButtonClicked(v: View) {
 
     }
+
+
+    fun clearButtonClicked(v: View) {
+
+    }
+
+    fun historyButtonClicked(v: View) {
+
+    }
+
 }
