0b61f5b863ea702d83d140da84c600cc40a7ff4a
Sohee <sohee3480@gmail.com>
2021-08-19 23:30:10 +0900

    History에 대해 저장한 것을 어떻게 이용할 것인지 설정해주는 곳

---
+++ app/src/main/java/com/example/aop_part2_chapter04/dao/HistoryDao.kt
@@ -0,0 +1,11 @@
+package com.example.aop_part2_chapter04.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.example.aop_part2_chapter04.model.History
+
+@Dao
+interface HistoryDao {
+
+
+}
