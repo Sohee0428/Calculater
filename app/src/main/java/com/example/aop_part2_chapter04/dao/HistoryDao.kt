ddd8382083907e12a95cc67107a47f3cf0517fbe
Sohee <sohee3480@gmail.com>
2021-08-19 23:32:50 +0900

    History의 정보를 저장하기

--- app/src/main/java/com/example/aop_part2_chapter04/dao/HistoryDao.kt
+++ app/src/main/java/com/example/aop_part2_chapter04/dao/HistoryDao.kt
@@ -1,6 +1,7 @@
 package com.example.aop_part2_chapter04.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import com.example.aop_part2_chapter04.model.History
 
@@ -10,6 +11,7 @@ interface HistoryDao {
     @Query("SELECT * FROM history")
     fun getAll(): List<History>
 
-
+    @Insert
+    fun insertHistory(history: History)
 
 }
