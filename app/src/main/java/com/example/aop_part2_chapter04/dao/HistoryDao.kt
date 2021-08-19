88440d468b465d822778cd4b3756355361bf4e0b
Sohee <sohee3480@gmail.com>
2021-08-19 23:34:43 +0900

    History의 정보 중 일부만 삭제

--- app/src/main/java/com/example/aop_part2_chapter04/dao/HistoryDao.kt
+++ app/src/main/java/com/example/aop_part2_chapter04/dao/HistoryDao.kt
@@ -1,6 +1,7 @@
 package com.example.aop_part2_chapter04.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.aop_part2_chapter04.model.History
@@ -17,5 +18,7 @@ interface HistoryDao {
     @Query("DELETE FROM history")
     fun deleteAll()
 
+    @Delete
+    fun delete(history: History)
 
 }
